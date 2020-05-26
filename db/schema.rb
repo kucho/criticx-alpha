face2bebdf8ffe996efedda2f10edb1a981a3d3f
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-26 10:45:55 -0500

    Created models and join tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,87 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_26_154316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "start_date"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.text "summary"
+    t.date "release_date"
+    t.integer "category"
+    t.decimal "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games_genres", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "genre_id", null: false
+  end
+
+  create_table "games_platforms", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "platform_id", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "involved_companies", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.bigint "game_id", null: false
+    t.boolean "developer"
+    t.boolean "publisher"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_involved_companies_on_company_id"
+    t.index ["game_id"], name: "index_involved_companies_on_game_id"
+  end
+
+  create_table "platforms", force: :cascade do |t|
+    t.string "name"
+    t.integer "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.date "birth_date"
+    t.integer "review_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "games_genres", "games"
+  add_foreign_key "games_genres", "genres"
+  add_foreign_key "games_platforms", "games"
+  add_foreign_key "games_platforms", "platforms"
+  add_foreign_key "involved_companies", "companies"
+  add_foreign_key "involved_companies", "games"
+  add_foreign_key "reviews", "users"
 end

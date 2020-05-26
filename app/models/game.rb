4db713aca0d67b32f9aa85a7420e5e9f9d8820e6
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-26 11:44:37 -0500

    Created self join association on Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   has_many :involved_companies
   has_many :companies, through: :involved_companies
+  has_many :expansions, class_name: 'Game', foreign_key: :parent_id
   has_and_belongs_to_many :platforms
   has_and_belongs_to_many :genres
+  belongs_to :parent, class_name: 'Game', optional: true
 end

307768e52741bd1beceea37d6aaad1809a7d67c8
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-26 19:41:47 -0500

    Added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user, counter_cache: :review_count
   belongs_to :reviewable, polymorphic: true
+
+  validates :title, presence: true, length: { maximum: 40 }, uniqueness: true
+  validates :body, presence: true
 end
